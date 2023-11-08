a74815b4c401b485ea84e0e6b35b029622cb87c5
Mark Marasigan <kramlivingstone@gmail.com>
2023-11-08 16:15:36 +0800

    Updated next.config.js file.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["s3.us-west-2.amazonaws.com"],
+    domains: [
+      "s3.us-west-2.amazonaws.com",
+      "prod-files-secure.s3.us-west-2.amazonaws.com",
+    ],
   },
   env: {
     notionKey: process.env.NOTION_TOKEN,
