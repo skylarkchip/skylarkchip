94e41833feed9d8b0b9b71eb17eb4d4629b761f0
Mark Marasigan <kramlivingstone@gmail.com>
2024-03-21 11:19:46 +0800

    Added Analytics and SpeedInsights components from Vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 import "@/styles/globals.css";
@@ -24,6 +26,8 @@ export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
     </ChakraProvider>
   );
 }
