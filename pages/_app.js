f37d1a822987f1b02d144907a58c391112140fc9
Mark Marasigan <kramlivingstone@gmail.com>
2024-03-21 11:22:49 +0800

    Updated import

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
